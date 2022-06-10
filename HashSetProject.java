cf710de73d2660cbf9bd229613641b720811b121
s25770-pj <92208994+s25770-pj@users.noreply.github.com>
2022-06-10 22:33:17 +0200

    6.2

---
+++ HashSetProject.java
@@ -0,0 +1,17 @@
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HashSetProject {
+    public static void main(String[] args) {
+        Set<String> Imiona = new HashSet<String>();
+        Imiona.add("Monika");
+        Imiona.add("Ania");
+        Imiona.add("Damian");
+        Imiona.add("Filip");
+            System.out.println(Imiona);
+            String Imiona = new String[] {1,2,3};
+    }
+}
